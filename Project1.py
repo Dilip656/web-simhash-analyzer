b3f776ad0799170d618bde8a4039e7dc37989720
unknown <diliprameshvaishnav@gmail.com>
2026-02-23 22:48:43 +0530

    Updated simhash logic and improved hashing implementation

--- Project1.py
+++ Project1.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import sys
+import re
 
 def fetching_html(url):
     try:
@@ -17,44 +18,40 @@ def parsing_page(html):
     links= [a["href"]for a in soup_.find_all("a",href=True)]
     return title,body,links
 
-def word_freq(text):
-    freq = {}
-    word =""
-    for ch in text:
-        if ch.isalnum():
-            word+=ch.lower()
-        else:
-            if word:
-                freq[word] = freq.get(word,0)+1
-                word = ""
-    if word:
-        freq[word] = freq.get(word, 0) +1
-    return freq
+def tokenize(text):
+    return re.findall(r"[a-zA-Z0-9]+",text.lower())
+def freq_map(word):
+    freq_m= {}
+    for w in word:
+        freq_m[w] = freq_m.get(w,0)+1
+    return freq_m
 
 P = 53
 Mask = (1<<64)  -1
 
 def polynomial_hash(word):
-    val = 0
-    power = 1
+    h = 0
     for ch in word:
-        val = (val+ord(ch)*power)&Mask
-        power = (power*P) & Mask
-    return val
+        h = (h * P + ord(ch)) & Mask
+    return h
 def simhash(freq):
     vector = [0]*64
     for word, weight in freq.items():
         h = polynomial_hash(word)
-        for i in range(64):
-            if(h>> i) & 1:
-                vector[i] += weight
+        bit_index = 0
+        temp = h
+        while bit_index < 64:
+            if temp & 1:
+                vector[bit_index] += weight
             else:
-                vector[i] -= weight
-    result = 0
-    for i in range(64):
-        if vector[i] > 0:
-            result |= (1 << i)
-    return result
+                vector[bit_index]-= weight
+            temp >>= 1
+            bit_index += 1
+    final_hash = 0
+    for i, score in enumerate(vector):
+        if score > 0:
+            final_hash |= (1 << i)
+    return final_hash
 
 def bits_common(h1,h2):
     xor = h1^h2
@@ -78,11 +75,11 @@ if len(sys.argv)==2:
 elif len(sys.argv)==3:
     html1 = fetching_html(sys.argv[1])
     html2 = fetching_html(sys.argv[2])
-    body1=parsing_page(html1)
-    body2 = parsing_page(html2)
+    _,body1,_=parsing_page(html1)
+    _,body2,_ = parsing_page(html2)
 
-    freq1 = word_freq(body1)
-    freq2 = word_freq(body2)
+    freq1 = freq_map(tokenize(body1))
+    freq2 = freq_map(tokenize(body2))
 
     simhash1 = simhash(freq1)
     simhash2 = simhash(freq2)
